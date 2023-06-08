fa4c243fcbf7739ca501164d71ca109aed91be5e
taufiqyfirdaus <taufiqyfirdaus@gmail.com>
2023-06-08 11:01:28 +0700

    fix route2

--- routes/web.php
+++ routes/web.php
@@ -12,6 +12,7 @@ use App\Http\Controllers\UserController;
 use App\Models\Province;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\TransactionController;
 
 /*
 |--------------------------------------------------------------------------
@@ -31,13 +32,15 @@ Route::get('/', [HomepageController::class, 'index'])->name('homepage');
 Route::get('/penginapan', [HomepageController::class, 'showPenginapan'])->name('showPenginapan');
 Route::get('/{province}/{city}/{content}', [HomepageController::class, 'detailContent'])->name('detailContent');
 Route::get('/budaya/{province}/{city}/{budaya}', [HomepageController::class, 'detailBudaya'])->name('detailBudaya');
-Route::get('/{content}/{penginapan}', [HomepageController::class, 'detailPenginapan'])->name('detailPenginapan');
+Route::get('/penginapan/{content}/{penginapan}', [HomepageController::class, 'detailPenginapan'])->name('detailPenginapan');
 Route::get('/province/{province}', [HomepageController::class, 'getContentProvince'])->name('getContentProvince');
 Route::get('/provinsi/{province}', [HomepageController::class, 'getBudayaProvince'])->name('getBudayaProvince');
 Route::get('/province', [HomepageController::class, 'getProvince'])->name('getProvince');
-Route::get('/provinsi', [HomepageController::class, 'getProvinceBudaya'])->name('getProvinceBudaya');
+Route::get('/provinsi', [HomepageController::class,'getProvinceBudaya'])->name('getProvinceBudaya');
 Route::get('/result', [HomepageController::class, 'result'])->name('result');
-
+Route::get('/transaction/{id}', [TransactionController::class, 'index'])->name('transaksi');
+Route::get('/transaction', [TransactionController::class, 'details'])->name('details');
+Route::post('/transaction/store', [TransactionController::class, 'store'])->name('store');
 
 Auth::routes();
 // Route::get('logout', [LoginController::class, 'logout']);
@@ -106,3 +109,5 @@ Route::get('/penginapan/{penginapan}', [PenginapanController::class, 'show'])->n
 Route::put('/administrator/penginapan/{penginapan}/edit', [PenginapanController::class, 'update'])->name('penginapan.update');
 Route::delete('administrator/penginapan/{penginapan}/delete', [PenginapanController::class, 'destroy'])->name('penginapan.destroy');
 Route::get('/administrator/penginapan/{penginapan}/edit', [PenginapanController::class, 'edit'])->name('penginapan.edit');
+    
+    // });
